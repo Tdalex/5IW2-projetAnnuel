7bc1bf62bdbec7f5df0038172619e9e17d1f4c6b
Unknown <yassir.chakiri12@gmail.com>
2017-12-06 23:37:08 +0100

    Stylish map

--- src/AppBundle/Resources/public/js/googleMap.js
+++ src/AppBundle/Resources/public/js/googleMap.js
@@ -5,48 +5,171 @@
 function myMap() {
     var mapProp= {
         center:new google.maps.LatLng(46.719208,1.474055),
-        zoom:5,
+        zoom:7,
         mapTypeId: google.maps.MapTypeId.ROADMA
     };
+
+    // Créer un nouveau style de map
+    var styledMapType = new google.maps.StyledMapType(
+        [
+            {
+                "featureType": "administrative",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#6195a0"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#f2f2f2"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#e6f3d6"
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 45
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#f4d2c5"
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "color": "#4e4e4e"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#f4f4f4"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#787878"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#eaf6f8"
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#eaf6f8"
+                    }
+                ]
+            }
+        ],
+        {name: 'Styled Map'});
+
     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-    // //création du marqueur
-    // var marqueur = new google.maps.Marker({
-    //     position: new google.maps.LatLng(48.856393, 2.343472),
-    //     map: map,
-    //     draggable: true
-    // });
-    //
-    // //chemin du tracé du polyline
-    // var parcours = [
-    //     new google.maps.LatLng(48.856393, 2.343472),
-    //     new google.maps.LatLng(47.950028, 1.907178),
-    //     new google.maps.LatLng(45.835296, 4.768425),
-    //     new google.maps.LatLng(43.299219, 5.365324)
-    // ];
-    //
-    // var raodtrip = new google.maps.Polyline({
-    //     path: parcours,//chemin du tracé
-    //     strokeColor: "#FF0000",//couleur du tracé
-    //     strokeOpacity: 1.0,//opacité du tracé
-    //     strokeWeight: 2//grosseur du tracé
-    // });
-    //
-    // //lier le tracé à la carte
-    // //ceci permet au tracé d'être affiché sur la carte
-    // raodtrip.setMap(map);
-    //
-    // //Lier un evenement au clic du marquer
-    // google.maps.event.addListener(marqueur, 'click', function() {
-    //     //Fenetre d'information
-    //     var infowindow =  new google.maps.InfoWindow({
-    //         title: "Titre",
-    //         content: 'Hello World!',
-    //         map: map,
-    //         position: new google.maps.LatLng(48.856393, 2.343472)
-    //     });
-    //     infowindow.open(map, this);
-    // });
+    //Associer le style a la map et l'afficher.
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
 
     //Tableau des marqueurs
     var tabMarqueurs = [];
