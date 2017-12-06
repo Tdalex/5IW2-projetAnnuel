952bd989840d0fe39e62d4926453854b51bd3ef2
Unknown <yassir.chakiri12@gmail.com>
2017-12-07 00:22:20 +0100

    Tracer itinéraire entre les marqueurs

--- src/AppBundle/Resources/public/js/googleMap.js
+++ src/AppBundle/Resources/public/js/googleMap.js
@@ -171,6 +171,45 @@ function myMap() {
     map.mapTypes.set('styled_map', styledMapType);
     map.setMapTypeId('styled_map');
 
+    // //création du marqueur
+    // var marqueur = new google.maps.Marker({
+    //     position: new google.maps.LatLng(48.856393, 2.343472),
+    //     map: map,
+    //     draggable: true
+    // });
+    //
+    // //chemin du tracé du polyline
+    // var parcours = [
+    //     new google.maps.LatLng(48.856393, 2.343472),
+    //     new google.maps.LatLng(47.950028, 1.907178),
+    //     new google.maps.LatLng(45.835296, 4.768425),
+    //     new google.maps.LatLng(43.299219, 5.365324)
+    // ];
+    //
+    // var raodtrip = new google.maps.Polyline({
+    //     path: parcours,//chemin du tracé
+    //     strokeColor: "#FF0000",//couleur du tracé
+    //     strokeOpacity: 1.0,//opacité du tracé
+    //     strokeWeight: 2//grosseur du tracé
+    // });
+    //
+    // //lier le tracé à la carte
+    // //ceci permet au tracé d'être affiché sur la carte
+    // raodtrip.setMap(map);
+    //
+    // //Lier un evenement au clic du marquer
+    // google.maps.event.addListener(marqueur, 'click', function() {
+    //     //Fenetre d'information
+    //     var infowindow =  new google.maps.InfoWindow({
+    //         title: "Titre",
+    //         content: 'Hello World!',
+    //         map: map,
+    //         position: new google.maps.LatLng(48.856393, 2.343472)
+    //     });
+    //     infowindow.open(map, this);
+    // });
+
+
     //Tableau des marqueurs
     var tabMarqueurs = [];
     var parcours = [];
@@ -192,6 +231,27 @@ function myMap() {
         for(i=0;i<parcours.length;i++) {
             var point =new google.maps.LatLng(parcours[i].split(',')[0],parcours[i].split(',')[1]);
             traceParcours.push(point);
+        }
+
+        //Tracer l'itinéraire entre les marqueurs
+        for(i=0;i<traceParcours.length;i++) {
+            // trajet 1
+            var directionsService = new google.maps.DirectionsService();
+            var directionsDisplay = new google.maps.DirectionsRenderer({ 'map': map });
+            var request = {
+                origin : traceParcours[i],
+                destination: traceParcours[i+1],
+                travelMode : google.maps.DirectionsTravelMode.DRIVING,
+                unitSystem: google.maps.DirectionsUnitSystem.METRIC
+            };
+
+            directionsService.route(request, function(response, status) {
+                if (status == google.maps.DirectionsStatus.OK) {
+                    directionsDisplay.setDirections(response);
+                    directionsDisplay.suppressMarkers = true;
+                    directionsDisplay.setOptions({polylineOptions:{strokeColor: '#008000'}, preserveViewport: true});
+                }
+            });
         }
 
         //créer le polyline
