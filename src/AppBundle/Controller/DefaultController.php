5697227125682ecc15cecd111d22866dfffd593e
Unknown <marinealexandre5@gmail.com>
2018-01-19 09:40:17 +0100

    form search index complete

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -74,7 +74,7 @@ class DefaultController extends Controller
 
         $roadtrips = $em->getRepository('AppBundle:Roadtrip')->searchRtDepDest($dep, $dest);
         foreach ($roadtrips as $key => $value){
-            $data['key']['url'] = $this->generateUrl('roadtrip_show', array('slug' => $roadtrips['key']['slug']));
+            $roadtrips[$key]['url'] = $this->generateUrl('roadtrip_show', array('slug' => $roadtrips[$key]['slug']));
         }
         $roadtrips = $this->get('serializer')->serialize($roadtrips, 'json');
 
