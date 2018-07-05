df20044f1da643233f7e9dfb74f7dbfcf1ea58ab
Unknown <yassir.chakiri12@gmail.com>
2018-07-05 17:57:02 +0200

    get lat & lng from address partner

--- src/AppBundle/Resources/public/js/waypoint.js
+++ src/AppBundle/Resources/public/js/waypoint.js
@@ -2,9 +2,10 @@
  * Created by mohamedchakiri on 05/07/2018.
  */
 
- var autocomplete;
+var autocomplete;
+var geocoder;
 
- function initAutocomplete() {
+function initAutocomplete() {
     var options = {
         componentRestrictions: {country: "fr"},
         types: ['geocode']
@@ -13,4 +14,18 @@
     var waypointInputs = document.getElementById('waypoint_address');
     var autocomplete = new google.maps.places.Autocomplete(waypointInputs, options);
     autocomplete.inputId = waypointInputs.id;
- }
+}
+
+function geocodeAddress(){
+    var address = document.getElementById('waypoint_address').value;
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == 'OK') {
+            alert(results[0].geometry.location);
+            var lat = results[0].geometry.location.lat;
+            var lng = results[0].geometry.location.lng;
+        } else {
+            alert('Geocode n\'a pas abouti car : ' + status);
+        }
+    });
+}
