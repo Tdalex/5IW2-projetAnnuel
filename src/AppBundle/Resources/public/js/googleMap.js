7147e73b835254bd0655e1cc0aa581ac14b9edf2
Unknown <yassir.chakiri12@gmail.com>
2018-07-14 19:03:28 +0200

    info window places

--- src/AppBundle/Resources/public/js/googleMap.js
+++ src/AppBundle/Resources/public/js/googleMap.js
@@ -352,6 +352,11 @@ function apiPlaces(bounds, type, icon) {
                     scaledSize: new google.maps.Size(25, 20)
                 }
             });
+            // If the user clicks a hotel marker, show the details of that hotel
+            // in an info window.
+            markers[i].placeResult = response.data[i];
+            console.log(markers[i].placeResult);
+            google.maps.event.addListener(markers[i], 'click', showInfoWindowApi);
             setTimeout(
                 dropMarker(i, markers)
                  , i * 100);
@@ -431,6 +436,13 @@ function clearResults(tbody) {
     }
 }
 
+//Show info for palces from api
+function showInfoWindowApi() {
+    var marker = this;
+    infoWindow.open(map, marker);
+    buildIWContentApi(marker.placeResult);
+}
+
 // Get the place details for a hotel. Show the information in an info window,
 // anchored on the marker for the hotel that the user selected.
 function showInfoWindow() {
@@ -443,6 +455,56 @@ function showInfoWindow() {
             infoWindow.open(map, marker);
             buildIWContent(place);
         });
+}
+
+// load inforamtion in window with data Api.
+function buildIWContentApi(place) {
+    /*document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' +
+        'src="' + place.icon + '"/>';*/
+    document.getElementById('iw-url').innerHTML = '<b>' + place.name + '</b>';
+    document.getElementById('iw-address').textContent = place.address;
+
+    if (place.phone) {
+        document.getElementById('iw-phone-row').style.display = '';
+        document.getElementById('iw-phone').textContent =
+            place.phone;
+    } else {
+        document.getElementById('iw-phone-row').style.display = 'none';
+    }
+
+    // Assign a five-star rating to the hotel, using a black star ('&#10029;')
+    // to indicate the rating the hotel has earned, and a white star ('&#10025;')
+    // for the rating points not achieved.
+    if (place.rating) {
+        var ratingHtml = '';
+        for (var i = 0; i < 5; i++) {
+            if (place.rating < (i + 0.5)) {
+                ratingHtml += '&#10025;';
+            } else {
+                ratingHtml += '&#10029;';
+            }
+            document.getElementById('iw-rating-row').style.display = '';
+            document.getElementById('iw-rating').innerHTML = ratingHtml;
+        }
+    } else {
+        document.getElementById('iw-rating-row').style.display = 'none';
+    }
+
+    // The regexp isolates the first part of the URL (domain plus subdomain)
+    // to give a short URL for displaying in the info window.
+    if (place.website) {
+        var hostnameRegexp = new RegExp('^https?://.+?/');
+        var fullUrl = place.website;
+        var website = hostnameRegexp.exec(place.website);
+        if (website === null) {
+            website = 'http://' + place.website + '/';
+            fullUrl = website;
+        }
+        document.getElementById('iw-website-row').style.display = '';
+        document.getElementById('iw-website').textContent = website;
+    } else {
+        document.getElementById('iw-website-row').style.display = 'none';
+    }
 }
 
 // Load the place information into the HTML elements used by the info window.
